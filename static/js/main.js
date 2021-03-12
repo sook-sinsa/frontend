5bec063832e98f40b925997d6f3e01454414c9d8
SOOYOUNG LIM <stellar1198@gmail.com>
2021-03-13 00:07:21 +0900

    add templates and static

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    var jbOffset = $('.nonfixed' ).offset();
+    console.log(jbOffset);
+    $(window).scroll(function(){
+      if ($(document).scrollTop() > jbOffset.top ) {
+        $('.nonfixed').addClass('fixed');
+      }
+      else {
+        $('.nonfixed' ).removeClass('fixed');
+      }
+    });
+  } );
